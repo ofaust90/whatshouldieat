8b23645eb6432857e5d0f85c8d5a808f1a55bb61
Oliver Faust <oliverfaust@MacBook-Pro-von-Oliver.local>
2023-04-02 10:21:56 +0200

    inital commit

---
+++ whatshouldieat-foodservice/src/main/java/ch/oliverfaust/whatshouldieatfoodservice/WhatshouldieatFoodserviceApplication.java
@@ -0,0 +1,13 @@
+package ch.oliverfaust.whatshouldieatfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatshouldieatFoodserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatshouldieatFoodserviceApplication.class, args);
+	}
+
+}
---
+++ whatshouldieat-foodservice/src/test/java/ch/oliverfaust/whatshouldieatfoodservice/WhatshouldieatFoodserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.oliverfaust.whatshouldieatfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatshouldieatFoodserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
